661bcff9f62c7d697a9dd4b3cb8e71ed7dc17900
IvanSlivenko <ivan8822@ukr.net>
2025-07-30 09:20:28 +0300

    30-07-2025-09-20

---
+++ start-new-project/src/index.ts
@@ -0,0 +1,13 @@
+// const express = require('express')
+import express, {Request, Response} from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+    let helloMessage = 'Hello World! 29.07.2025'
+    res.send(helloMessage)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
